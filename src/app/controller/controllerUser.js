81d6160dbde6ad955dd19828fae9e0132167b008
Antonio Machado <ulundoantonio@gmail.com>
2021-06-30 13:50:33 +0100

    acabei de configurar o controllerUser para o teste de envio sem fila

--- src/app/controller/controllerUser.js
+++ src/app/controller/controllerUser.js
@@ -1,3 +1,5 @@
+import Mail from '../lib/Mail'
+
 export default {
    async store (req,res){
        const { name, email,password } = req.body;
@@ -6,7 +8,13 @@ export default {
           name,
           email,
           password,
-       }
+       } 
+       await  Mail.sendMail({
+          from:'DLine-Code <auroradasilva@gmail.com',
+          to:`${name}` `<${email}>`,
+          subject:'TESTE DE ENVIO DE MENSAGEM POR EMAIL',
+          html:`ola, ${name}, seja bem-vindo a Dline-Code`
+       })
        return res.send({user})
    }
 }
