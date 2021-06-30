4c74f89141934dee55edce81b1914f32e73c4aa6
Antonio Machado <ulundoantonio@gmail.com>
2021-06-30 12:42:44 +0100

    criaei a pasta app para os s controllers

--- src/route.js
+++ src/route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import  controllerUser from './controller/controllerUser.js'
+import  controllerUser from './app/controller/controllerUser.js'
 const router = Router();
 
 router.post('/user',controllerUser.store)
